f4cbe99e6ba8f1ea733d4dfc447d0f1d8f8b548f
dqmurre <sauras777@gmail.com>
2025-05-30 21:06:11 +0200

    make build

---
+++ Museum.be5814ba.js
@@ -0,0 +1,2 @@
+let swiper=new Swiper(".image-slider",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},autoHeight:!0,slidesPerView:"auto",spaceBetween:20});
+//# sourceMappingURL=Museum.be5814ba.js.map
